512d44dc997da35c30c8a86fbcf084ceed119929
Kunal <kunal456k@gmail.com>
2022-07-05 01:31:09 +0530

    PRWatcher project initial commit

---
+++ app/src/main/java/com/kunal456k/prwatcher/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kunal456k.prwatcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
